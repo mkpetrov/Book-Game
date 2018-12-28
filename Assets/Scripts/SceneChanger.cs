e9d58604e32d264a9f080ba0f2c1197c5a704234
mkpetrov <mkpetrov@abv.bg>
2018-12-28 09:58:31 +0200

    Start of the Book-Game

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene : MonoBehaviour {
+
+	public void ChangeVisibility(bool isVisible)
+    {
+        if (isVisible)
+        {
+            this.gameObject.SetActive(true);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
